d4f3b7009b18d26d3bacbf4fa0e7c967a0e61203
Nate Jones <nate@endot.org>
2014-02-05 19:37:48 -0800

    separate pinger from reporter with a channel

--- cli.go
+++ cli.go
@@ -5,10 +5,32 @@ import (
 	"time"
 )
 
+type Result struct {
+	when  time.Time
+	value bool
+}
+
 func main() {
 
+	channel := make(chan Result)
+
+	go pinger(channel)
+	go reporter(channel)
+
+	// sleep forever
+	select {}
+}
+
+func pinger(reporter chan Result) {
 	c := time.Tick(1 * time.Second)
 	for now := range c {
-		fmt.Println(now.Format("03:04:05"))
+		reporter <- Result{when: now, value: false}
+	}
+}
+
+func reporter(source chan Result) {
+	for {
+		result := <-source
+		fmt.Println(result.when.Format("03:04:05"))
 	}
 }
